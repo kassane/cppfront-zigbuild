ea6d1b979d568ad2c6b191a572bac1616d0e4f6f
Matheus Catarino <matheus-catarino@hotmail.com>
2025-04-11 10:43:52 -0300

    updated to zig v0.14.0

--- build.zig
+++ build.zig
@@ -9,26 +9,40 @@ pub fn build(b: *std.Build) void {
     const cpp2file = b.option([]const u8, "cpp2", "Input - Cpp2 Source file") orelse "examples/pure2-hello.cpp2";
     const cpp2pure = b.option(bool, "pure", "Allow Cpp2 syntax only [default: false]") orelse false;
 
-    const cppfront = cpp2cpp1(b, .{
+    const example_name = std.fs.path.basename(cpp2file[0 .. cpp2file.len - 5]);
+
+    var cpp2config: cpp2Config = .{
         .file = cpp2file,
         .pure = cpp2pure,
         .dependency = cppfront_dep,
         .target = target,
         .optimize = optimize,
-    });
+    };
+    const cppfront = cpp2cpp1(b, &cpp2config);
     const cpp2_step = b.step("cppfront", "Run cppfront build (cpp2 -> cpp1)");
     cpp2_step.dependOn(&cppfront.step);
 
-    example_build(b, .{
-        .file = cpp2file,
-        .target = target,
-        .optimize = optimize,
-        .dependency = cppfront_dep,
-    });
+    if (cpp2config.output_cpp) |cpp| {
+        const example = example_build(b, .{
+            .file = cpp,
+            .filename = example_name,
+            .target = target,
+            .optimize = optimize,
+            .dependency = cppfront_dep,
+        });
+
+        const run_cmd = b.addRunArtifact(example);
+        if (b.args) |args| {
+            run_cmd.addArgs(args);
+        }
+
+        const run_step = b.step("run", "Run C++ example code");
+        run_step.dependOn(&run_cmd.step);
+    }
 }
 
 // convert cpp2 to cpp
-fn cpp2cpp1(b: *std.Build, config: cpp2Config) *std.Build.Step.Run {
+pub fn cpp2cpp1(b: *std.Build, config: *cpp2Config) *std.Build.Step.Run {
     const exe = b.addExecutable(.{
         .name = "cppfront",
         .target = config.target,
@@ -53,25 +67,27 @@ fn cpp2cpp1(b: *std.Build, config: cpp2Config) *std.Build.Step.Run {
     if (b.verbose)
         cppfront_exec.addArg("-verb");
 
-    cppfront_exec.addArgs(&.{
-        config.file,
-        "-o",
+    cppfront_exec.addFileArg(b.path(config.file));
+    cppfront_exec.addArg("-o");
+    config.output_cpp = cppfront_exec.addOutputFileArg(
         config.file[0 .. config.file.len - 1], // rename cpp2 to cpp
-    });
+    );
 
     cppfront_exec.step.dependOn(b.getInstallStep());
     return cppfront_exec;
 }
 const cpp2Config = struct {
     file: []const u8,
+    output_cpp: ?std.Build.LazyPath = null,
     pure: bool,
     dependency: *std.Build.Dependency,
     optimize: std.builtin.OptimizeMode,
     target: std.Build.ResolvedTarget,
 };
-fn example_build(b: *std.Build, info: BuildInfo) void {
+
+fn example_build(b: *std.Build, info: BuildInfo) *std.Build.Step.Compile {
     const example = b.addExecutable(.{
-        .name = info.filename(),
+        .name = info.filename,
         .target = info.target,
         .optimize = info.optimize,
     });
@@ -79,7 +95,7 @@ fn example_build(b: *std.Build, info: BuildInfo) void {
     if (example.rootModuleTarget().os.tag == .windows)
         example.want_lto = false;
     example.addCSourceFile(.{
-        .file = b.path(b.fmt("examples/{s}.cpp", .{info.filename()})),
+        .file = info.file,
         .flags = cflags,
     });
     example.addIncludePath(info.dependency.path("include"));
@@ -88,24 +104,15 @@ fn example_build(b: *std.Build, info: BuildInfo) void {
     else
         example.linkLibC();
 
-    const run_cmd = b.addRunArtifact(example);
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
-    }
-
-    const run_step = b.step("run", "Run C++ example code");
-    run_step.dependOn(&run_cmd.step);
+    return example;
 }
 
 const BuildInfo = struct {
-    file: []const u8,
+    file: std.Build.LazyPath,
+    filename: []const u8,
     optimize: std.builtin.OptimizeMode,
     target: std.Build.ResolvedTarget,
     dependency: *std.Build.Dependency,
-    pub fn filename(self: BuildInfo) []const u8 {
-        var split = std.mem.splitAny(u8, std.fs.path.basename(self.file), ".");
-        return split.first();
-    }
 };
 
 const cflags = &.{
